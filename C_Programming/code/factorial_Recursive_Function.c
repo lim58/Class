12dc783891c59da9cf2df1a07d964b75f8479ab9
limdayoung <dylim7715@gmail.com>
2023-06-12 20:10:21 +0900

    create : factorial_Recursive_Function

---
+++ C_Programming/code/factorial_Recursive_Function.c
@@ -0,0 +1,19 @@
+// 재귀함수 사용하여 팩토리얼 구하기
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int func(int num)
+{
+    if (num == 0) return;
+    return num * func(num - 1);
+}
+
+int main()
+{
+    int n;
+    printf("자연수를 입력하세요 : ");
+    scanf("%d", &n);
+    printf("%d! = %d", n, func(n));
+
+    return 0;
+}
