b44a6f835200707d87631bcaf4193b471b65eeb7
lim58 <dylim7715@gmail.com>
2023-09-29 01:14:53 +0900

    create : web_programming_var

---
+++ Web_Programming/JavaScript_Code/var.js
@@ -0,0 +1,63 @@
+let a;
+console.log(a);
+
+aa = 1000;
+console.log(aa);
+
+// node var
+// npm install nodemon -g
+// nodemon var
+
+let intiger = 123;
+let negative = -123;
+let double = 1.23;
+
+console.log(intiger);
+console.log(negative);
+console.log(double);
+console.clear();
+
+let binary = 0b1111011;
+let octal = 0o173;
+let hex = 0x76;
+
+console.log(binary);
+console.log(octal);
+console.log(hex);
+
+console.log(0 / 123);
+console.log(123 / 0);
+console.log(123 / -0);
+
+let bigInt = 1234567890123456789012345678n;
+console.log(bigInt);
+
+let string = "안녕";
+console.log(string);
+string = "'안녕'";
+console.log(string);
+string = "안녕\n 반가워요 \t\t 잘가요";
+console.log(string);
+
+let id = "길동";
+let greetings = "반갑습니다";
+console.log(`홍${id} 안녕하세요 ${greetings}`);
+
+let isFree = true;
+let isActivated = false;
+console.log(!!0);
+console.log(!!-0);
+console.log(!!"");
+console.log(!!null);
+console.log(!!undefined);
+console.log(!!NaN);
+
+console.log(!!1);
+console.log(!!-1);
+console.log(!!"text");
+console.log(!!{});
+console.log(!!Infinity);
+
+let f = 100;
+console.log(typeof f);
+
