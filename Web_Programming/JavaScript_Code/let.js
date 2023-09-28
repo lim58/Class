b14520d80fceb5e833c96671426622887008d738
lim58 <dylim7715@gmail.com>
2023-09-29 01:14:31 +0900

    create : web_programming_let

---
+++ Web_Programming/JavaScript_Code/let.js
@@ -0,0 +1,18 @@
+// 객체 object
+let apple = {};
+0;
+console.log(apple);
+console.log(typeof apple);
+let sos = { name: "soso", color: "red" };
+console.log(sos);
+console.log(sos.name);
+
+console.clear();
+
+// 자동형 변환
+let num1 = "10";
+let num2 = 20;
+console.log(num1 + num2);
+console.log(num2 - num1);
+console.log(num2 * num1);
+console.log(num2 / num1);
